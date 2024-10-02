a693bb745b87f0c108469022f9a6dd440ca521ce
AP1506 <ameliapercival2004@gmail.com>
2024-10-02 10:18:54 -0400

    Next level + timer

--- main.gd
+++ main.gd
@@ -6,13 +6,29 @@ extends Node
 @onready var curse_screen = $MiniGame/CurseScreen
 @onready var camera = $World/SubViewport/Level/Camera2D
 @onready var player = $World/SubViewport/Player
+
 @onready var escaped_label = $UI2/EscapedLabel
+@onready var time_label = $UI2/TimeLabel
+
+@onready var game_over_label = $Popups/GameOver/BoxContainer/GameOverLabel
+@onready var time_elapsed_popup_label = $Popups/GameOver/BoxContainer/HBoxContainer/TimeElapsedLabel
+@onready var best_time_label = $Popups/GameOver/BoxContainer/HBoxContainer/BestTimeLabel
+
+@onready var retry_button = $Popups/GameOver/BoxContainer/HBoxContainer2/RetryButton
+@onready var next_level_button = $Popups/GameOver/BoxContainer/HBoxContainer2/NextLevelButton
 
 var current_level = 1 # The current level, corresponding to the keys in level_information
 var enemies_escaped = 0:
 	set(value):
 		enemies_escaped = value
 		escaped_label.text = "Escaped " + String.num_int64(value) + "/" + String.num_int64($"/root/GlobalVariables".level_information[current_level]["escapes_allowed"] + 1)
+var all_enemies_defeated = false
+
+var timer = 0:
+	set(value):
+		timer = value
+		time_label.text = "Time elapsed: " + String.num_int64(int(value) / 60) + ":" + ("0" if int(value) % 60 < 10 else "") + String.num_int64(int(value) % 60)
+var timer_on = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -33,6 +49,8 @@ func _ready():
 	
 	enemies_escaped = 0
 	
+	timer = 0
+	
 	# Enemy set up
 	for enemy in get_tree().get_nodes_in_group("enemies"):
 		enemy.player = player
@@ -52,21 +70,34 @@ func _process(delta):
 	else:
 		$Guidebook.visible = true
 
+func _physics_process(delta):
+	if timer_on:
+		timer += delta
+
 func _on_player_died():
 	game_over.bind("You died!").call_deferred()
 
+# For winning and losing conditions
 func game_over(game_end_reason : String):
 	$World.process_mode = Node.PROCESS_MODE_DISABLED
 	$Popups/GameOver/BoxContainer/GameOverLabel.text = game_end_reason
-	$Popups/GameOver/BoxContainer/RetryButton.disabled = false
+	retry_button.disabled = false
+	timer_on = false
 	
 	curse_screen.take_down_screen()
+	curse_controller.curse_state = CurseController.CurseState.NON_ACTIVE
+	
+	time_elapsed_popup_label.text = "Time elapsed: " + String.num_int64(int(timer) / 60) + ":" + ("0" if int(timer) % 60 < 10 else "") + String.num_int64(int(timer) % 60)
+	if (timer < $"/root/GlobalVariables".level_information[current_level]["best_completion_time"]):
+		$"/root/GlobalVariables".level_information[current_level]["best_completion_time"] = timer
 	
 	$Popups.visible = true
 	$Popups/GameOver.visible = true
 
-func reload_level():
-	print("Reloaded the level " + $"/root/GlobalVariables".level_information[current_level]["path"])
+func load_level(level_number : int):
+	print("Loaded the level " + $"/root/GlobalVariables".level_information[level_number]["path"])
+	
+	current_level = level_number
 	
 	$Popups.visible = false
 	
@@ -76,7 +107,7 @@ func reload_level():
 	# Move player out of level, remove level, and reload it
 	player.reparent($World/SubViewport)
 	$World/SubViewport/Level.free()
-	$World/SubViewport.add_child(load($"/root/GlobalVariables".level_information[current_level]["path"]).instantiate(), true)
+	$World/SubViewport.add_child(load($"/root/GlobalVariables".level_information[level_number]["path"]).instantiate(), true)
 	camera = $World/SubViewport/Level/Camera2D
 	
 	set_camera_limits()
@@ -89,6 +120,7 @@ func reload_level():
 	player.new_level()
 	
 	enemies_escaped = 0
+	timer = 0
 	
 	# Enemy set up
 	for enemy in get_tree().get_nodes_in_group("enemies"):
@@ -97,12 +129,19 @@ func reload_level():
 		player.connect_on_attacked(enemy.attack_area.area_entered, enemy)
 		enemy.tree_exited.connect(_on_enemy_exited_tree)
 	
-	$World.process_mode = Node.PROCESS_MODE_INHERIT
-	
 	$AudioStreamPlayer.restart_playing()
+	
+	# Allow inputs
+	timer_on = true
+	curse_controller.curse_state = CurseController.CurseState.ACTIVE
+	$World.process_mode = Node.PROCESS_MODE_INHERIT
+
+func reload_level():
+	load_level(current_level)
 
 func load_next_level():
 	print("Loaded the next level ")
+	load_level(current_level + 1 if $"/root/GlobalVariables".level_information.keys().size() < current_level else current_level)
 
 func _on_escaped(area: Node):
 	if area.get_parent() is Enemy:
@@ -123,8 +162,14 @@ func _on_enemy_exited_tree():
 			print("Enemies still exist on map")
 	else:
 			print("Cleared all enemies on the map")
+			game_over.bind("Success!").call_deferred()
 
 func _on_retry_button_pressed():
-	$Popups/GameOver/BoxContainer/RetryButton.disabled = true
+	retry_button.disabled = true
 	$AudioStreamPlayer.stop_playing()
 	reload_level.call_deferred()
+
+
+func _on_next_level_button_pressed():
+	
+	load_next_level.call_deferred()
